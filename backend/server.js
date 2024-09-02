53bf19830ada877e208ba0e782ee4043b56e0ed7
Obada-Jawabreh <obadajawabreh6@gmail.com>
2024-09-02 13:59:04 +0300

    login and sign up

--- backend/server.js
+++ backend/server.js
@@ -3,38 +3,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Connect to MongoDB
 const mongoURI = process.env.MONGO_URI;
-mongoose.connect(mongoURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(mongoURI).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.error('Failed to connect to MongoDB', err);
 });
 
+app.use(express.json());
 
-
+const userRoutes = require('./routes/users')
+app.use("/api/users", userRoutes);
 
 
 // Middleware
-app.use(express.json());
-
-
-
-
-
-
 
 // Basic route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
