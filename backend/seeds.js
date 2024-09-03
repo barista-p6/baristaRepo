260c2c1f0d4ed08c76ce05b046ca909a616bfd83
TasnimZaid <tasnim.aboarqob@gmail.com>
2024-09-03 13:53:48 +0300

    linktoResipeDetails

--- backend/seeds.js
+++ backend/seeds.js
@@ -13,10 +13,14 @@ require('dotenv').config();
 
 
 
+
 mongoose.connect(process.env.MONGO_URI, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true
 });
+
+
+
 async function seed() {
   try {
     await mongoose.connection.dropDatabase();
